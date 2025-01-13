0523db204456863cf4a5739341bb99a4639ba207
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2025-01-13 23:02:34 +0700

    feat: statistic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Activity;
+use App\Models\Cause;
+use App\Models\Flood;
 use App\Models\Meta;
 use App\Models\Post;
+use App\Models\Risk;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -18,6 +22,14 @@ class HomeController extends Controller
             "meta" => $this->meta(),
             "posts" => Post::orderBy('id', 'DESC')->limit(3)->get(),
             "activities" => Activity::orderBy('id', 'DESC')->limit(4)->get(),
+            "count_activity" => Activity::all()->count(),
+            "count_cause" => Cause::all()->count(),
+            "count_flood" => Flood::all()->count(),
+            "count_post" => Post::all()->count(),
+            "count_risk" => Risk::all()->count(),
+            "count_user" => User::all()->count(),
+
+
         ]);
     }
 
--- resources/views/sections/services.blade.php
+++ resources/views/sections/services.blade.php
@@ -3,41 +3,44 @@
 
   <!-- Section Title -->
   <div class="container section-title" data-aos="fade-up">
-    <h2>Fitur</h2>
-    <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+    <h2>Statistik CegahBanjir</h2>
+    <p>Temukan gambaran lengkap untuk aksi mitigasi banjir</p>
   </div><!-- End Section Title -->
 
   <div class="container">
 
     <div class="row gy-4">
 
-      <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
-        <div class="card col-12 service-item position-relative">
-          <div class="icon"><i class="bi bi-geo-alt icon"></i></div>
-          <h4><a href="/beranda#home-map" class="stretched-link">Peta penyebab banjir</a></h4>
-        </div>
-      </div>
+      @php
+        $statistics = [
+          (object) ['name' => 'Penyebab banjir',      'icon' => 'bi bi-geo-alt',                  'count' => $count_cause,],
+          (object) ['name' => 'Daerah rawan banjir',  'icon' => 'bi bi-map-fill',                 'count' => $count_risk,],
+          (object) ['name' => 'Kolaborator',          'icon' => 'bi bi-person-fill',              'count' => $count_user,],
+          (object) ['name' => 'Laporan',              'icon' => 'bi bi-file-earmark-text-fill',   'count' => $count_risk+$count_flood,],
+          (object) ['name' => 'Artikel edukasi',      'icon' => 'bi bi-book-half',                'count' => $count_post,],
+          (object) ['name' => 'Kegiatan',             'icon' => 'bi bi-calendar2-week',           'count' => $count_activity,],
+        ];
+      @endphp
 
-      <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="200">
-        <div class="card col-12 service-item position-relative">
-          <div class="icon"><i class="bi bi-map-fill icon"></i></div>
-          <h4><a href="service-details.html" class="stretched-link">Peta daerah rawan banjir</a></h4>
+      @foreach ($statistics as $s)
+        <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="{{$loop->iteration}}00">
+          <div class="card col-12 service-item position-relative py-3">
+            
+            <div class="row">
+              <div class="col-3">
+                <div class="icon"><i class="{{$s->icon}} icon"></i></div>
+              </div>
+              <div class="col-9">
+                <p class="fs-3 mb-2">{{$s->count}}</p>
+                <h4 class="mb-0 fs-6"><a href="/beranda#home-map" class="stretched-link">{{$s->name}}</a></h4>
+              </div>
+            </div>
+          
+          </div>
         </div>
-      </div>
+      @endforeach
 
-      <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="300">
-        <div class="card col-12 service-item position-relative">
-          <div class="icon"><i class="bi bi-book-half icon"></i></div>
-          <h4><a href="service-details.html" class="stretched-link">Artikel edukasi</a></h4>
-        </div>
-      </div>
-
-      <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="400">
-        <div class="card col-12 service-item position-relative">
-          <div class="icon"><i class="bi bi-calendar2-week icon"></i></div>
-          <h4><a href="service-details.html" class="stretched-link">Informasi kegiatan</a></h4>
-        </div>
-      </div>
+     
 
     </div>
 

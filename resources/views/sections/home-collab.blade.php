9a2f0509273bc7510964167baf4fa880568029a8
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2025-01-14 23:13:12 +0700

    Update home-collab.blade.php

--- resources/views/sections/home-collab.blade.php
+++ resources/views/sections/home-collab.blade.php
@@ -8,7 +8,7 @@
 
   <div class="container">
 
-    <div class="row text-center">
+    <div class="row text-center justify-content-center">
 
       @foreach ($users as $u)
       <div class="col-md-3">
@@ -39,42 +39,11 @@
         </a>
       </div>
       @endforeach
-      
-      {{-- @for ($i = 1; $i < 4; $i++)
-      <div class="col-md-4">
-        <div class="card position-relative rounded-5">
-          <div class="card-body p-4 text-white rounded-5" style="background-color: #30604C; padding-bottom: 50px !important">
-            <div class="rounded-4 mb-3" style="height: 250px; background-color: #D9D9D9"></div>
-            <div class="my-5" style="color: #C1E0F7">
-              <p class="mb-1">Nama</p>
-              <p class="mb-0">
-                <i class="bi bi-geo-alt"></i> Lokasi
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          class="h1 mx-auto rounded-circle"
-          style="width: 100px;
-          height: 100px;
-          line-height: 100px;
-          background-color: #619DB5;
-          color: #30604C;
-          font-weight: 600;
-          font-size: 72px;
-          margin-top: -50px;
-          z-index: 1000;
-          position: relative;"
-        >
-          {{$i}}
-        </div>
-      </div>
-      @endfor --}}
 
     </div>
     <br>
     <div class="text-center">
-      <button class="btn btn-primary">Beri Kontribusi</button>
+      <a class="btn btn-success" href="/kolaborator">Beri Kontribusi</a>
     </div>    
 
   </div>

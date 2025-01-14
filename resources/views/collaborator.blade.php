193420f01d52784358e5ef6d64aa946974cf9c37
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2025-01-14 22:33:05 +0700

    fix bug

--- resources/views/collaborator.blade.php
+++ resources/views/collaborator.blade.php
@@ -10,7 +10,7 @@
 
       <div class="col-lg-8 row">
         <div class="col-md-4">
-          <img class="rounded-circle" src="/assets/img/user/{{$user->image}}" style="width: 100%">
+          <img class="rounded-circle" src="/assets/img/user/{{ $user->image ? $user->image : 'default.webp' }}" style="width: 100%">
         </div>
         <div class="col-md-8">
           <p class="mb-0 fw-bold">{{$user->name}} ({{$user->type}})</p>
@@ -26,7 +26,7 @@
       <div class="col-lg-4">
         <h3 class="fw-bold mt-4 mb-3" style="font-size: 20px;">Collaborator terbaru</h3>
         @foreach ($users as $u)
-        <a href="/collaborator/{{$u->username}}" data-aos="fade-up" data-aos-delay="200" >
+        <a href="/kolaborator/{{$u->username}}" data-aos="fade-up" data-aos-delay="200" >
           <div class="card mb-3" style="border-radius:20px; padding: 20px">
             <p class="mb-0">{{$u->name}}</p>
           </div>
